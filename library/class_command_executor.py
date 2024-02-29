419febdd396fe7fada3ca78bc113733621d82e66
Minjune <mjsong2021@gmail.com>
2024-02-28 23:14:18 -0500

    attempt to print accelerate dumpts to stdout

--- library/class_command_executor.py
+++ library/class_command_executor.py
@@ -1,6 +1,7 @@
 import subprocess
 import psutil
 from library.custom_logging import setup_logging
+import threading
 
 # Set up logging
 log = setup_logging()
@@ -9,14 +10,21 @@ log = setup_logging()
 class CommandExecutor:
     def __init__(self):
         self.process = None
-
     def execute_command(self, run_cmd):
-        if self.process and self.process.poll() is None:
-            log.info(
-                'The command is already running. Please wait for it to finish.'
-            )
-        else:
-            self.process = subprocess.Popen(run_cmd, shell=True)
+            if self.process and self.process.poll() is None:
+                log.info('The command is already running. Please wait for it to finish.')
+            else:
+                # Using subprocess.PIPE allows capturing the stdout and stderr
+                self.process = subprocess.Popen(run_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+                # Start threads to read stdout and stderr without blocking
+                threading.Thread(target=self._read_stream, args=(self.process.stdout,)).start()
+                threading.Thread(target=self._read_stream, args=(self.process.stderr,)).start()
+
+    def _read_stream(self, stream):
+        for line in stream:
+            print(line, end='')  # Print each line from the stream to stdout
+        stream.close()
 
     def kill_command(self):
         if self.process and self.process.poll() is None:
